47a63f016fcfbab9cb2b2b1b8822d1007de0e2d7
DeviousLab <deviouslab@gmail.com>
2022-04-25 00:18:28 +0400

    Implemented basic error handling
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
@@ -12,3 +16,5 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
