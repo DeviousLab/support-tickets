c2fd289eb4a21cebb08514bf0c7f5600bc1e250e
DeviousLab <deviouslab@gmail.com>
2022-04-24 23:49:34 +0400

    Basic express server setup
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
