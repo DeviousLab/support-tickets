c2275fa8c4c24a4a2d76325a98df1332945b1c7b
DeviousLab <deviouslab@gmail.com>
2022-04-26 23:50:52 +0400

    Implememented register functionality with redux
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,10 @@
-import React, { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FaUser } from 'react-icons/fa'
 import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { register, reset } from '../features/auth/authSlice';
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -11,6 +15,22 @@ function Register() {
   });
 
   const { name, email, password, confirmpassword } = formData;
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { user, isLoading, isSuccess, isError, message } = useSelector(state => state.auth);
+
+  useEffect(() => {
+    if(isError) {
+      toast.error(message);
+    }
+    if(isSuccess || user) {
+      toast.success("You have successfully registered");
+      navigate('/');
+    }
+    dispatch(reset());
+  }, [isError, isSuccess, user, message, dispatch, navigate]);
 
   const onChange = e => {
     setFormData(prevState => ({
@@ -23,6 +43,13 @@ function Register() {
     e.preventDefault();
     if (password !== confirmpassword) {
       toast.error('Passwords do not match');
+    } else {
+      const userData = {
+        name,
+        email,
+        password
+      }
+      dispatch(register(userData));
     }
   }
 
