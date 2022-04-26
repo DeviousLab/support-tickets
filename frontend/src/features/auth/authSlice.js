a76f5c2eb84370ee0d8174b02314b6a692843017
DeviousLab <deviouslab@gmail.com>
2022-04-27 00:00:42 +0400

    Implemented logout functionality with redux
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -2,8 +2,10 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 import authService from "./authService";
 
+const user = JSON.parse(localStorage.getItem("user"));
+
 const initialState = {
-  user: null,
+  user: user ? user: null,
   isError: false,
   isSuccess: false,
   isLoading: false,
@@ -22,6 +24,10 @@ export const register = createAsyncThunk('auth/register', async (user, thunkAPI)
 export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
   console.log(user);
 });
+
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout();
+})
 
 export const authSlice = createSlice({
   name: 'auth',
@@ -48,6 +54,9 @@ export const authSlice = createSlice({
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, (state) => {
         state.user = null;
       })
   }
