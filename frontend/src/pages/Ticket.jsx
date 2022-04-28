2ec8af50665a853bdea442a57ebbd6ddaaa1e11e
DeviousLab <deviouslab@gmail.com>
2022-04-29 01:22:32 +0400

    Implemented method to add note on tickets
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,17 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import Modal from 'react-modal';
+import { FaPlus } from 'react-icons/fa';
 
 import { getTicket, closeTicket } from '../features/tickets/ticketSlice';
-import { getNotes } from '../features/notes/noteSlice';
+import { getNotes, createNote } from '../features/notes/noteSlice';
 import { BackButton } from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import NoteItem from '../components/NoteItem';
 
+const customStyles = {
+  content: {
+    width: '600px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'relative',
+  },
+}
+
+Modal.setAppElement('#root')
+
 function Ticket() {
-  const { isLoading, isSuccess, ticket, isError, message } = useSelector(state => state.tickets);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [noteText, setNoteText] = useState('');
+
+  const { isLoading, ticket, isError, message } = useSelector(state => state.tickets);
   const { isLoading: notesLoading, notes } = useSelector(state => state.notes);
 
 
@@ -36,6 +56,15 @@ function Ticket() {
     navigate('/tickets');
   }
 
+  const onNoteSubmit = (e) => {
+    e.preventDefault()
+    dispatch(createNote({ noteText, ticketId }))
+    closeModal()
+  }
+
+  const openModal = () => setIsOpen(true)
+  const closeModal = () => setIsOpen(false)
+
   if (isLoading || notesLoading) {
     return <Spinner />
   }
@@ -46,7 +75,7 @@ function Ticket() {
 
   return (
     <div className='ticket-page'>
-            <header className='ticket-header'>
+      <header className='ticket-header'>
         <BackButton url='/tickets' />
         <h2>
           Ticket ID: {ticket._id}
@@ -55,7 +84,7 @@ function Ticket() {
           </span>
         </h2>
         <h3>
-          Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-GB')}
         </h3>
         <h3>Product: {ticket.product}</h3>
         <hr />
@@ -65,6 +94,41 @@ function Ticket() {
         </div>
         <h2>Notes</h2>
       </header>
+
+      {ticket.status !== 'closed' && (
+        <button onClick={openModal} className='btn'>
+          <FaPlus /> Add Note
+        </button>
+      )}
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel='Add Note'
+      >
+        <h2>Add Note</h2>
+        <button className='btn-close' onClick={closeModal}>
+          X
+        </button>
+        <form onSubmit={onNoteSubmit}>
+          <div className='form-group'>
+            <textarea
+              name='noteText'
+              id='noteText'
+              className='form-control'
+              placeholder='Note text'
+              value={noteText}
+              onChange={(e) => setNoteText(e.target.value)}
+            ></textarea>
+          </div>
+          <div className='form-group'>
+            <button className='btn' type='submit'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
 
       {notes.map((note) => (
         <NoteItem key={note._id} note={note} />
