bc8e2e9870f81fd614595a912dd8e19869b44faa
DeviousLab <deviouslab@gmail.com>
2022-04-29 01:37:08 +0400

    Setup changes to server before deployment
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
@@ -15,10 +16,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to Ticket System' })
+  });
+}
 app.use('/api/users', require('./routes/userRoutes'));
 
 app.use('/api/tickets', require('./routes/ticketRoutes'));
